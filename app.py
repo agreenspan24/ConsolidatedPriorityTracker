d3bafd142bd0b9e1eeb92570100873d8f4a8d849
Adam Greenspan <agreenspan24@gmail.com>
2018-10-07 13:08:49 -0500

    review null case

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zmteqmujvkfnmq:8dce67c5ae02028c174df35be856535c90a9593280ab43edceadf25f3f76f97a@ec2-54-83-204-230.compute-1.amazonaws.com:5432/de8kcah93s00d9'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['OIDC_CLIENT_SECRETS'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'client_secrets.json')
@@ -14,4 +14,4 @@ app.config['OIDC_SCOPES'] = ['openid', 'email', 'profile']
 app.config['OIDC_ID_TOKEN_COOKIE_SECURE'] = False
 db = SQLAlchemy(app)
 oid = OpenIDConnect()
-app.secret_key = os.environ['secret_key']
+app.secret_key = 'fasdjhkR#$@sdaf'
