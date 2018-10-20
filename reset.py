c0ad1f445cbcd5b3c33db874596a9fd0243022e8
Adam Greenspan <agreenspan24@gmail.com>
2018-10-20 10:38:33 -0500

    view schema

--- reset.py
+++ reset.py
@@ -4,18 +4,19 @@ from app import engine, db
 from setup_config import dashboard_query
 from helpers import update_shifts
 from backup import backup
+import os
 
 def main():
     backup()
 
-    engine.execute('DROP VIEW IF EXISTS consolidated.dashboard_totals')
+    engine.execute('DROP VIEW IF EXISTS ' + os.environ['schema'] + '.dashboard_totals')
     Note.__table__.drop(engine)
     Shift.__table__.drop(engine)
     CanvassGroup.__table__.drop(engine)
     CanvassGroup.__table__.create(engine)
     Shift.__table__.create(engine)
     Note.__table__.create(engine)
-    engine.execute(dashboard_query)
+    engine.execute(dashboard_query % os.environ['schema'])
 
     update_shifts()
     
