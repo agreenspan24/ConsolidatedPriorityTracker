540feb7db471341e78233539da33b64bd4c42cb9
Adam Greenspan <agreenspan24@gmail.com>
2018-10-20 18:05:21 -0500

    background color rows

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ from sqlalchemy_views import CreateView, DropView
 from flask import abort
 import os
 
-schema = os.environ['schema'] if os.environ['schema'] != None else 'consolidated'
+schema = os.environ['schema']
+#schema = 'consolidated'
 
 class SyncShift(db.Model):
     __table_args__ = {'schema':'sync'}
