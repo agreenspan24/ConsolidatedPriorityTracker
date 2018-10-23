58efbb26c5f61b9bb6547ef91bc2160d5ecc6b6a
Adam Greenspan <agreenspan24@gmail.com>
2018-10-23 10:37:51 -0500

    fixing calc

--- models.py
+++ models.py
@@ -488,13 +488,12 @@ class ShiftStats:
 class HeaderStats:
     def __init__(self, shifts, groups):
         time_now = datetime.now()
-        overdue = (time_now + timedelta(minutes=20)).time()
+        overdue = (time_now - timedelta(minutes=20)).time()
         flipped_status = ['In', 'Resched', 'No Show', 'Completed', 'Declined']
 
-        self.unflipped_shifts = sum(1 for x in shifts if x.time < overdue and x.status not in flipped_status)
+        self.unflipped_shifts = sum(1 for x in shifts if x.time < overdue and not x.status in flipped_status)
         self.overdue_check_ins = sum(1 for x in groups if not x.is_returned and x.check_in_time != None and x.check_in_time < time_now.time())
         self.flakes_not_chased = sum(1 for x in shifts if x.flake and x.status == 'No Show' and x.flake_pass < 1)
-
 
 
 class BackupGroup(db.Model):
