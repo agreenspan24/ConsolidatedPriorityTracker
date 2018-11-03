b011d594de8435a866fccf9b29d857aa593929c3
Adam Greenspan <agreenspan24@gmail.com>
2018-11-03 16:05:41 -0500

    fixing setup schema

--- setup_config.py
+++ setup_config.py
@@ -229,7 +229,7 @@ ORDER BY region, office
 
 users_query = """
 
-CREATE VIEW consolidated.users_view AS 
+CREATE VIEW {0}.users_view AS 
 SELECT users.id, email, first_name, last_name, region, office, rank
 , (current_date + MIN(time)) min_time
 , (current_date + MAX(time)) max_time
